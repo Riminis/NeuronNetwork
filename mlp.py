462a3e46c8c7b4718a38a4a80bbc2c5cd8928d38
Володя <118829074+Riminis@users.noreply.github.com>
2026-05-04 20:02:20 +0300

    Add files via upload

--- mlp.py
+++ mlp.py
@@ -10,24 +10,37 @@ class MLP():
             n_in = layers_dims[l]
             n_out = layers_dims[l + 1]
 
-            W = np.random.randn(n_out, n_in) * np.sqrt(1 / n_in)
-            b = np.zeros((1, n_out))
+            W = np.random.randn(n_out, n_in) * np.sqrt(2.0 / n_in)
+            b = np.random.randn(1, n_out) * 0.01
 
             self.weights.append(W)
             self.biases.append(b)
 
-        self.cache = {'A': []}
+        self.cache = {'A': [], 'Z': []}
+
+    def activation(self, x):
+        '''Функция активации - сигмоида'''
+        return np.maximum(x, 0)
+
+    def loss(self, y_pred, y):
+        '''Функция потерь - MSE'''
+        return 0.5 * ((y_pred - y) ** 2).sum() / len(y)
 
     def forward(self, X):
         '''Функция прохода по всей сети'''
-        self.cache['A'] = [X]
+        self.cache = {'A': [X], 'Z': []}
         A = X
 
         for l in range(len(self.weights)):
             z = (A @ self.weights[l].T) + self.biases[l]
-            a = self.activation(z)
+
+            if l == len(self.weights) - 1:
+                a = 1 / (1 + np.exp(-z))
+            else:
+                a = np.maximum(0, z)
 
             self.cache['A'].append(a)
+            self.cache['Z'].append(z)
             A = a
 
         return a
@@ -36,37 +49,37 @@ class MLP():
         '''Функция обратного распространения ошибки'''
         m = X.shape[0]
         grads = {'dW': [], 'db': []}
-
         L = len(self.weights) - 1
         A = self.cache['A'][L + 1]
         A_prev = self.cache['A'][L]
+        Z_last = self.cache['Z'][L]
 
         dA = (A - y) / m
-        dZ = dA * (A * (1 - A))
+
+        dZ = dA * A * (1 - A)
 
         dW = (dZ.T @ A_prev) / m
         db = np.sum(dZ, axis=0, keepdims=True) / m
-        
         grads['dW'].append(dW)
         grads['db'].append(db)
 
         for l in range(len(self.weights) - 2, -1, -1):
             A = self.cache['A'][l + 1]
             A_prev = self.cache['A'][l]
+            Z_curr = self.cache['Z'][l]
             W = self.weights[l + 1]
 
             dA = dZ @ W
-            dZ = dA * (A * (1 - A))
+
+            dZ = dA * (Z_curr > 0).astype(float)
 
             dW = (dZ.T @ A_prev) / m
             db = np.sum(dZ, axis=0, keepdims=True) / m
-            
             grads['dW'].append(dW)
             grads['db'].append(db)
 
         grads['dW'] = grads['dW'][::-1]
         grads['db'] = grads['db'][::-1]
-
         return grads
 
     def update_params(self, grads, learning_rate=0.01):
@@ -98,16 +111,8 @@ class MLP():
                 grads = self.backward(X_batch, y_batch)
                 self.update_params(grads, learning_rate)
 
-    def activation(self, x):
-        '''Функция активации - сигмоида'''
-        return 1 / (1 + np.exp(-x))
-
-    def loss(self, y_pred, y):
-        '''Функция потерь - MSE'''
-        return 0.5 * ((y_pred - y) ** 2).sum() / len(y)
-
 if __name__ == '__main__':
-    X = np.array([[0, 0],
+    X = np.array([[0, 0 ],
                  [0, 1],
                  [1, 0],
                  [1, 1]])
@@ -121,9 +126,9 @@ if __name__ == '__main__':
     model.fit(
         X=X,
         y=y,
-        epoch=10000,
+        epoch=500,
         batch_size=4,
-        learning_rate=1.0
+        learning_rate=2.0
         )
     print(f'Конец обучения')
     y_pred = model.forward(X)
